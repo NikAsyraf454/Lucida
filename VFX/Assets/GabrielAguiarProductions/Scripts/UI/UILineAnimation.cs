621ee17c72e464a5cf94f5d44067d1e8f0f9e3ca
Nik Asyraf <54460063+NikAsyraf454@users.noreply.github.com>
2022-10-01 22:37:43 +0800

    Graph prototype successful
    further development is needed (add dot or img of circle at each point of plot)
    
    animation is using leanTween

---
+++ VFX/Assets/GabrielAguiarProductions/Scripts/UI/UILineAnimation.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class UILineAnimation : MonoBehaviour
+{
+    public UILineRenderer[] lines;
+
+    public float time = 1f;
+
+    private void OnEnable()
+    {
+        AnimateLines();
+    }
+
+    public void AnimateLines()
+    {
+        foreach(UILineRenderer line in lines)
+        {
+            AnimateLine(line);
+        }
+    }
+
+    void AnimateLine(UILineRenderer line)
+    {
+        List<Vector2> points = line.points;
+
+        Animate(line, points);
+    }
+
+    public void Animate(UILineRenderer line, List<Vector2> points)
+    {
+        line.points = new List<Vector2>();
+
+        for(int i=0; i<points.Count; i++)
+        {
+            int index = i;
+            AnimatePoint(line, index, new Vector2(0, 4), points[index]);
+        }
+    }
+
+    void AnimatePoint(UILineRenderer line, int index, Vector2 start, Vector2 end)
+    {
+        LeanTween.delayedCall(time * index, () =>
+        {
+            if(index > 0)
+            {
+                start = line.points[index - 1];
+                line.points.Add(start);
+            }
+            else
+            {
+                line.points.Add(start);
+            }
+
+            LeanTween.value(gameObject, (value) =>
+            {
+                line.points[index] = value;
+                line.SetVerticesDirty();
+            }, start, end, time);
+        });
+
+    }
+
+
+}
